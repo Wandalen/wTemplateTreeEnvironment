11493758e176c5e65ff3f91f454db056f38338d5
wandalen <wandalen.me@gmail.com>
2017-10-15 22:00:25 +0400

    adjust sample

--- sample/Sample.js
+++ sample/Sample.js
@@ -1,9 +1,9 @@
 
 if( typeof module !== 'undefined' )
-require( 'wtemplatetree' );
+require( 'wtemplatetreeenvironment' );
 
 var tree = { 'a' : 'a', b : [ 1,2,3 ], c : { c1 : [ 1,2,3 ], c2 : [ 11,22,33 ] }, d : '{{^^a}}' }
-var templateTree = new wTemplateTree();
+var templateTree = new wTemplateTreeEnvironment();
 templateTree.tree = tree;
 
 var b1 = templateTree.query( 'b/1' );
