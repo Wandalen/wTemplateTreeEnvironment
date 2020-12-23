d9b85124e2ffec326ee38fb4e11427235fe2d2d0
Jack Shell <jackshel45@gmail.com>
2020-12-23 16:09:29 +0200

    Fixed

--- sample/Sample.s
+++ sample/Sample.s
@@ -6,8 +6,8 @@ var tree = { 'a' : 'a', 'b' : [ 1, 2, 3 ], 'c' : { 'c1' : [ 1, 2, 3 ], 'c2' : [ 
 var templateTree = new wTemplateTreeEnvironment();
 templateTree.tree = tree;
 
-var b1 = templateTree.query( 'b/1' );
-var d = templateTree.query( 'd' );
+var b1 = templateTree.select( 'b/1' );
+var d = templateTree.select( 'd' );
 
 console.log( 'b1 :', b1 );
 // b1 : 2
